b51ba8354cbac6abc0856fc56265411d7b5227af
juh <>
2017-07-10 14:02:07 -0300

    Primeiro commit

---
+++ AprendendoSistemas/src/banco/SQLQuery1.sql
@@ -0,0 +1,78 @@
+Create database teste
+
+create table estados(
+idestado int primary key identity,
+nomeEstado varchar(50) not null,
+siglaEstado varchar(2) not null
+)
+
+create table cidades(
+idcidade int primary key identity,
+nomeCidade varchar(80) not null,
+codEstado int not null,
+foreign key(codEstado) references estados(idestado)
+)
+
+create table bairro(
+idbairro int primary key identity,
+nomeBairro varchar(80) not null,
+codBairro int not null,
+Foreign key (codBairro) references cidades(idcidade)
+)
+
+create table telefone(
+idtelefone int primary key identity,
+numero varchar(15) not null,
+codCliente int not null,
+codFornecedor int not null,
+foreign key (codCliente) references cliente(idcliente),
+foreign key(codFornecedor) references fornecedor(idfornecedor)
+
+)
+
+create table cliente(
+idcliente int primary key identity,
+nomeCliente varchar(80) not null,
+endereco varchar(100) not null,
+rg varchar(15) not null,
+cpf  varchar(15) not null,
+codBairro int not null,
+codCidade int not null,
+foreign key(codBairro) references bairro(idbairro),
+foreign key (codCidade) references cidades(idcidade)
+)
+
+create table fornecedor(
+idfornecedor int primary key identity,
+nomeFornecedor varchar(80) not null,
+endereco varchar(100) not null,
+codBairro int not null,
+codCidade int not null,
+cnpjFornecedor varchar(30) not null,
+foreign key(codBairro) references bairro(idbairro),
+foreign key (codCidade) references cidades(idcidade)
+)
+create table produtos(
+idproduto int primary key identity,
+nomeProduto varchar(50) not null,
+precoVenda decimal(10,2) not null,
+precoCompra decimal(10,2) not null,
+codFornecedor int not null,
+foreign key(codFornecedor) references fornecedor(idfornecedor)
+)
+
+create table venda(
+idvenda int primary key identity,
+dataVenda varchar(10) not null,
+valorVenda decimal(10,2) not null,
+codCliente int not null,
+foreign key (codCliente) references cliente(idcliente)
+)
+
+create table itensProdutos(
+idvenda int not null,
+idproduto int not null,
+primary key(idvenda,idproduto),
+foreign key (idvenda) references venda(idvenda),
+foreign key(idproduto) references produtos(idproduto)
+)
---
+++ AprendendoSistemas/src/utilitarios/ConectaBanco.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilitarios;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author MAQLABiii
+ */
+public class ConectaBanco {
+   public  Statement atm;//responsavel por preparar e realizar pesquisas no banco de dados 
+   public  ResultSet rs;// responsavel por armazenar o resultado de uma pesquisa passada para o statement 
+   private  String driver = "net.sourceforge.jtds.jdbc.Driver";// responsavel por identificar o serviço de banco de dados
+    private String caminho = "jdbc:jtds:sqlserver://MAQLABIII-PC:1433/teste";// responsavel por setar o local do banco de dados
+    private String usuario= "sa";
+    private String senha = "dba";
+    public Connection con; // responsavel pela conexao do banco de dados
+
+public void conexao(){//método responsavel por realizar a conexao com o banco de dados
+    
+       try {
+           System.setProperty("jdbc.Drivers", driver);// seta a propriedade do driver de conexao
+           con = DriverManager.getConnection(caminho, usuario, senha);// realiza a conexao com o banco de dados
+     JOptionPane.showMessageDialog(null, "Conectado com sucesso");
+       } catch (SQLException ex) { // excessão 
+          JOptionPane.showMessageDialog(null, "Erro de conexao!\n Erro:"+ ex.getMessage());
+       }
+
+    }
+  
+public void desconecta(){
+       try {
+           con.close();
+       } catch (SQLException ex) {
+          JOptionPane.showMessageDialog(null, "Erro ao fechar a conexao!\n Erro:"+ ex.getMessage());
+       
+       }
+}
+}
